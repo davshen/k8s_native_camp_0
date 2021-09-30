4700ddc04c7d38f5eabc8a8ec1208050bb7f659b
shendawei@kanzhun.com <shendawei@kanzhun.com>
2021-09-30 18:36:01 +0800

    [update] httpserver:v1.1

--- httpserver/main.go
+++ httpserver/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	//"github.com/golang/glog"
 )
@@ -13,14 +14,31 @@ import (
 func main() {
 	flag.Set("v", "4")
 	//glog.V(2).Info("Starting http server...")
+	log.Println("start")
 	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/healthz", healthz)
 	http.HandleFunc("/sayhelloName", sayhelloName)
+	http.HandleFunc("/addHeader", addHeader)
 	fmt.Println("Runing...")
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+}
+
+func addHeader(w http.ResponseWriter, r *http.Request) {
+	req_header := (map[string][]string)(r.Header)
+	for k, arr := range req_header {
+		w.Header().Add(k, arr[0])
+	}
+	fmt.Fprintf(w, "req_Header全部数据：", req_header)
+	w.Header().Add("key", "dawei")
+	fmt.Fprintf(w, "\nreponse_Header全部数据：", w.Header())
+
+	var VERSION string
+	VERSION = os.Getenv("VERSION")
+	fmt.Println("VERSION", VERSION)
 
 }
 
@@ -38,7 +56,8 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 }
 
 func healthz(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "200\n   ")
+	io.WriteString(w, "ok!\n   ")
+	w.WriteHeader(200)
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
